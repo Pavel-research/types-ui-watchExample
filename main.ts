bfef4d256bbabf3b65362649ef9835096eb1b28f
Pavel Petrochenko <petrochenko.pavel.a@gmail.com>
2017-03-15 07:51:33 +0600

    working on documentation

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
 import RC=require("types-ui");
 import teams=require("./teams")
+RC.register(teams);
 var bnd=RC.render(document.getElementById("example"),teams.types.Team,{name:"Pavel",lastName:"Petrochenko"},{})
